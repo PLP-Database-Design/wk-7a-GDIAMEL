cb9907a3ee382b7949f5b80db5cf58c63b6ae259
GDIAMEL <manusam4090@gmail.com>
2025-05-08 00:48:11 +0300

    WEEK SEVEN

---
+++ answers.sql
@@ -0,0 +1,56 @@
+-- QUESTION ONE 
+-- FIRST NORMAL FROM
+-- ANSWER
+
+-- Creating the 1NF-compliant table
+CREATE TABLE ProductDetail_1NF (
+    OrderID INT,
+    CustomerName VARCHAR(100),
+    Product VARCHAR(100)
+);
+
+-- Inserting atomic values that is one product per row
+INSERT INTO ProductDetail_1NF (OrderID, CustomerName, Product)
+VALUES
+(101, 'John Doe', 'Laptop'),
+(101, 'John Doe', 'Mouse'),
+(102, 'Jane Smith', 'Tablet'),
+(102, 'Jane Smith', 'Keyboard'),
+(102, 'Jane Smith', 'Mouse'),
+(103, 'Emily Clark', 'Phone');
+
+-- QUESTION TWO
+-- SECOND NORMAL FORM
+-- ANSWER
+
+-- Creating the Orders table which has no partial dependency
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerName VARCHAR(100)
+);
+
+-- Creating the OrderDetails table that is dependent on full primary key
+CREATE TABLE OrderDetails_2NF (
+    OrderID INT,
+    Product VARCHAR(100),
+    Quantity INT,
+    PRIMARY KEY (OrderID, Product),
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+);
+
+-- Inserting values into Orders
+INSERT INTO Orders (OrderID, CustomerName)
+VALUES
+(101, 'John Doe'),
+(102, 'Jane Smith'),
+(103, 'Emily Clark');
+
+-- Inserting values into OrderDetails_2NF
+INSERT INTO OrderDetails_2NF (OrderID, Product, Quantity)
+VALUES
+(101, 'Laptop', 2),
+(101, 'Mouse', 1),
+(102, 'Tablet', 3),
+(102, 'Keyboard', 1),
+(102, 'Mouse', 2),
+(103, 'Phone', 1);
